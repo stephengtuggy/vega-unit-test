a8db2793b3c60c77872df3ceb41087690217ac47
Stephen G Tuggy <sgt@stephengtuggy.com>
2022-10-02 15:52:07 -0700

    *.cpp, *.hpp: Add copyleft notice

--- main.cpp
+++ main.cpp
@@ -1,3 +1,20 @@
+// Copyright (c) 2022 Stephen G. Tuggy
+//
+// This file is part of Vega Test.
+//
+// Vega Test is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Vega Test is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Vega Test. If not, see <https://www.gnu.org/licenses/>.
+
 #include <iostream>
 #include "boost/format.hpp"
 #include <boost/smart_ptr/shared_ptr.hpp>
